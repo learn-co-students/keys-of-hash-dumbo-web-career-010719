efc13158f945d61deeb64eaca74d4e73a0098eb8
github username <github email address>
2018-12-17 12:48:46 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
-class Hash
-  def keys_of(arguments)
-    # code goes here
+require 'pry'
+class Hash 							# inside of a ruby method, you can use self to refer to the object that the method was called on
+  def keys_of(*arguments)			# In this case, a method runs on a particular hash
+    self.map do |k, v|				# And you can refer to the hash with self
+    	if arguments.include?(v)
+    		k
+	    # binding.pry
+	    end
+    end.compact
   end
 end
