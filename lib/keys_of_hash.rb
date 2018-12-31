190f5083cd878cd31072e0db1fc3650521c5deff
github username <github email address>
2018-12-31 15:44:51 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,31 @@
+# returns an array
+# returns red-footed tortoise in an array for the animal
+# hash when passed the argument 'Panama'
+
+# returns two species in an array for the animal
+# hash when passed the argument 'Madagascar'
+
+# returns three species in an array for the animal hash
+# when passed the argument 'Australia'
+
+# returns the keys of the entered value
+# returns the keys of the entered value
+
+# can accept multiple arguments (values) and
+# return the keys that match one value or the other
+
+
+require"pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    new_arr = []
+    arguments.each do |argument|
+      self.each do |k,v|
+        if v == argument
+          new_arr << k
+        end
+      end
+    end
+      return new_arr
   end
 end
